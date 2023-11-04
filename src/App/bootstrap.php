e7408ae700f578565a2ce3514a60059ec269e94b
Adnan <adnan@Adnans-MBP.home>
2023-11-04 14:20:26 +0100

    initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/** composer autoload dependencies */
+ require __DIR__ . "/../../vendor/autoload.php";
+
+ use Framework\App;
+ $app = new App();
+
+ return $app; 
